57082352e779fa3de3c548b4ec44f3a7837a570a
Chuck Forsyth <charles.forsyth@ucr.edu>
2022-10-25 04:14:06 +0000

    blast slurm script update

--- blast/test_job.sh
+++ blast/test_job.sh
@@ -1,11 +1,30 @@
 #!/bin/bash -l
 
-#SBATCH -c 10
-#SBATCH --mem=10g
-#SBATCH --time=2:00:00
-#SBATCH -p short
+#SBATCH -c 4 
+#SBATCH -p compute 
 
-module load ncbi-blast
-cd ~/bigdata/Projects/blast_fasta/
-blastp -num_threads 10 dsg sdgsdg dhfdh
 
+project="blast"
+ran=$(echo $RANDOM)
+
+filename=$project"_"$ran
+
+echo $filename
+
+scratchdir=$HOME/$filename
+
+mkdir -p $scratchdir
+
+cp ./cluster-data/* $scratchdir/.
+
+module load blast-2.13
+
+cd $scratchdir
+time makeblastdb -in mouse.1.protein.faa -dbtype prot
+time makeblastdb -in zebrafish.1.protein.faa -dbtype prot
+
+time blastp -query zebrafish.1.protein.faa -db mouse.1.protein.faa -num_threads 4 -out zebrafish.x.mouse
+#blastp -query zebrafish.top.faa -db mouse.1.protein.faa -num_threads 4 -out zebrafish.x.mouse
+
+tar -zcvf zebrafish-job-output.tar.gz zebrafish.x.mouse
+cp zebrafish-job-output.tar.gz $HOME/$filename-zebrafish-job-output.tar.gz
