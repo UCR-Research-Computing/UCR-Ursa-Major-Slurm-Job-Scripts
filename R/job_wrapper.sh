7cf7c627e1449783a97d9ac024317f575626a91c
Chuck Forsyth <charles.forsyth@ucr.edu>
2023-02-14 01:33:21 +0000

    update

--- R/job_wrapper.sh
+++ R/job_wrapper.sh
@@ -1,18 +1,15 @@
 #!/bin/bash -l
 
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=10G
-#SBATCH --time=1-00:15:00     # 1 day and 15 minutes
 #SBATCH --mail-user=useremail@address.com
 #SBATCH --mail-type=ALL
 #SBATCH --job-name="R Example"
-#SBATCH -p intel # This is the default partition, you can use any of the following; intel, batch, highmem, gpu
 
 # The latest R is loaded by default
-# However, if you want to use a diferent version, then do so here
-#module load R
+module load r
+module load r-rjags
+module load r-coda
+module load r-lattice
 
 # Use Rscript to run R script
 Rscript myRscript.R
+Rscript jags.R
